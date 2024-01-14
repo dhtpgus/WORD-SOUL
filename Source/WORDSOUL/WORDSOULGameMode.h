fbe6b1a4b3f535aef97649c690eb5ed4cf78b01a
dhtpgus <dhtpgus1414@gmail.com>
2024-01-14 16:43:23 +0900

    Initial Project

---
+++ Source/WORDSOUL/WORDSOULGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WORDSOULGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWORDSOULGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AWORDSOULGameMode();
+};
+
+
+
