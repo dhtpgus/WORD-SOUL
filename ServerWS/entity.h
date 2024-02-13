0ede1b48680a3d1c003c6e043d6516fcefceb415
Cho Yong Joon <yzz1108j@gmail.com>
2024-02-14 05:12:23 +0900

    entity updated

--- ServerWS/entity.h
+++ ServerWS/entity.h
@@ -1,8 +1,39 @@
 #pragma once
+#include <atomic>
+#include "packet.h"
 
-class Entity {
-public:
-private:
-	float x_, y_, z_;
-	short hp_;
-};
+namespace entity {
+	extern std::atomic_uint id;
+
+	struct Postion {
+		float x, y, z;
+	};
+
+	class Entity {
+	public:
+		Entity(float x, float y, float z, short hp) 
+			: pos_{ x, y, z }, hp_{ hp }, id_{ id.fetch_add(1) } {}
+
+		void SetPostion(float x, float y, float z) {
+			pos_.x = x;
+			pos_.y = y;
+			pos_.z = z;
+		}
+
+		void SetPostion(packet::Position p) {
+			SetPostion(p.x, p.y, p.z);
+		}
+
+		unsigned int GetID() {
+			return id_;
+		}
+		const Postion& GetPostion() const {
+			return pos_;
+		}
+
+	private:
+		unsigned int id_;
+		Postion pos_;
+		short hp_;
+	};
+}
