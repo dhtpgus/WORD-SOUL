6f84ea7d2ca225118631d7c2b208428b4efce854
dhtpgus <dhtpgus1414@gmail.com>
2024-06-20 23:43:18 +0900

    Add Breakable actor

--- Source/WORDSOUL/Public/Enemy/Enemy.h
+++ Source/WORDSOUL/Public/Enemy/Enemy.h
@@ -22,7 +22,7 @@ public:
 	AEnemy();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-	virtual void GetHit(const FVector& ImpactPoint) override;
+	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
 	virtual void Destroyed() override;
 
